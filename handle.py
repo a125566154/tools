b0c03636b94d2b40f07eddf9650b61c46c0f277d
cary wang <carywang@carys-MacBook.local>
2019-03-22 10:37:12 +0800

    modify handle.py

--- handle.py
+++ handle.py
@@ -11,7 +11,7 @@ class Handle(object):
             timestamp = data.timestamp
             nonce = data.nonce
             echostr = data.echostr
-            token = "xxxx" #请按照公众平台官网\基本配置中信息填写
+            token = "1qaz2wsxE" #请按照公众平台官网\基本配置中信息填写
 
             list = [token, timestamp, nonce]
             list.sort()
