013bed931e5a7280777110916520e8a0856b7caf
cary wang <carywang@carys-MacBook.local>
2019-03-23 23:41:51 +0800

    handle

--- handle.py
+++ handle.py
@@ -72,7 +72,7 @@ class Handle(object):
                         """ % toUser
                         token = basic.Basic().get_access_token()
                         reply_template = template.Template()
-                        reply_template.send(data)
+                        reply_template.send(data, token)
                         return data
             else:
                 print "暂且不处理"
