0ff5ad32a30b06ab1d3d350a290c86d016eff7c9
cary wang <carywang@carys-MacBook.local>
2019-03-25 23:48:45 +0800

    index,app

--- app.py
+++ app.py
@@ -6,11 +6,16 @@ render = web.template.render('views/')
 urls = (
     '/', 'index',
     '/wx','Handle',
+    '/bus', 'bus',
 )
 
 class index:
     def GET(self):
         return render.index()
+
+class bus:
+    def GET(self):
+        return render.bus()
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
